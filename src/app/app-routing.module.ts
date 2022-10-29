5e6c9208b0b6fc673c1092c6eb198715cd8f0a83
Ignacio1-at <ignacio.torres.g@mail.pucv.cl>
2022-10-29 19:53:11 -0300

    EP3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistroComponent } from './registro/registro.component';
+import { MenuComponent } from './menu/menu.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/login', pathMatch: 'full'},
   {path: 'login', component:LoginComponent},
-  {path : 'registro', component:RegistroComponent}
+  {path : 'registro', component:RegistroComponent},
+  {path : 'menu', component:MenuComponent}
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, FormControl, FormArray, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'SaveLife';
+
+  public formParent: FormGroup = new FormGroup({}); //Declaracion del FORM
+
+
 }
+
